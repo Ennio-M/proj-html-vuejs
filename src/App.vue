e88858d640c0698901ceca34e4ae90a7a8ab6196
Ennio-M <marrese.ennio@gmail.com>
2022-05-17 16:20:59 +0200

    struttura dati

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <h1 class="text-center">AVADA FOOD</h1>
+    <app-header :data="data"/>
   </div>
 </template>
 
 <script>
-
+import data from './data.json';
+import AppHeader from './components/AppHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    AppHeader
+  },
+  data() {
+    return {
+      data: data
+    }
   }
 }
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <h2 v-for="item in data.navbar" :key="item.id">{{item.text}}</h2>
+        <i v-for="(social, index) in data.socials" :key="index" :class="social"></i>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    props: {
+        data: Object
+    }    
+}
+</script>
+
+<style lang="scss">
+    
+</style>
